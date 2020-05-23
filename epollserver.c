f3879db28349adba2fbcc0992bdc8a32931cdee0
Samuel Michael Squire <sam@samsquire.com>
2020-05-23 01:22:30 +0100

    Sam move server to thread

--- epollserver.c
+++ epollserver.c
@@ -12,10 +12,11 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <netdb.h>
-#include <sys/fcntl.h> 
+#include <sys/fcntl.h>
 #include <sys/stat.h>
-#include <sys/ioctl.h>      
-#include <unistd.h>     
+#include <sys/ioctl.h>
+#include <unistd.h>
+#include <pthread.h>
 
 #define PORT "9034"   // port we're listening on
 
@@ -24,7 +25,9 @@
 #define MAX_EPOLL_EVENTS 64
 #define MAX_EVENTS 10
 
-int do_use_fd(int i, fd_set master, int *fdmax, int listener) {
+
+
+int do_use_fd(int epollfd, struct epoll_event ev, int i, fd_set master, int *fdmax, int listener) {
     char buf[256];    // buffer for client data
     int nbytes;
     if ((nbytes = recv(i, buf, sizeof buf, 0)) <= 0) {
@@ -35,8 +38,17 @@ int do_use_fd(int i, fd_set master, int *fdmax, int listener) {
 	} else {
 	    perror("recv");
 	}
+
+        ev.events = EPOLLIN | EPOLLOUT;
+        ev.data.fd = i;
+       if (epoll_ctl(epollfd, EPOLL_CTL_DEL, i, &ev) == -1) {
+    	   perror("epoll_ctl: conn_sock");
+    	   exit(EXIT_FAILURE);
+       }
+
 	close(i); // bye!
 	FD_CLR(i, &master); // remove from master set
+
     } else {
 	// we got some data from a client
 	for(int j = 0; j <= *fdmax; j++) {
@@ -53,6 +65,7 @@ int do_use_fd(int i, fd_set master, int *fdmax, int listener) {
 	}
     }
 }
+
 int setunblocking(int fd)
 {
     int flags;
@@ -68,7 +81,73 @@ int setunblocking(int fd)
     flags = 1;
     return ioctl(fd, FIOBIO, &flags);
 #endif
-}  
+}
+
+typedef struct worker_t {
+    int epollfd;
+    int nfds;
+    int n;
+    int listener;
+    int fdmax;
+    socklen_t addrlen;
+    struct epoll_event ev;
+    struct epoll_event events[MAX_EVENTS];
+    struct sockaddr_storage remoteaddr;
+    int conn_sock;
+    fd_set master;
+} worker_t;
+
+void * run_worker(void * args) {
+    worker_t *worker_t = args;
+
+    int epollfd = worker_t->epollfd;
+    struct epoll_event ev = worker_t->ev;
+    struct epoll_event * events = worker_t->events;
+    int nfds = worker_t->nfds;
+    int n = worker_t->n;
+    int listener = worker_t->listener;
+    socklen_t addrlen = worker_t->addrlen;
+    struct sockaddr_storage remoteaddr = worker_t->remoteaddr;
+    int conn_sock = worker_t->conn_sock;
+    int fdmax = worker_t->fdmax;
+    fd_set master = worker_t->master;
+   for (;;) {
+       nfds = epoll_wait(epollfd, events, MAX_EVENTS, -1);
+       if (nfds == -1) {
+	   perror("epoll_wait");
+	   exit(EXIT_FAILURE);
+       }
+
+       for (n = 0; n < nfds; ++n) {
+	   if (events[n].data.fd == listener) {
+	       printf("Socket ready\n");
+	       addrlen = sizeof remoteaddr;
+	       conn_sock = accept(listener,
+		(struct sockaddr *)&remoteaddr,
+		&addrlen);
+	       if (conn_sock == -1) {
+		   perror("accept");
+		   exit(EXIT_FAILURE);
+	       }
+	       ev.events = EPOLLIN | EPOLLET;
+	       ev.data.fd = conn_sock;
+	       setunblocking(conn_sock);
+	       if (conn_sock > fdmax) {
+			fdmax = conn_sock;
+	       }
+	       if (epoll_ctl(epollfd, EPOLL_CTL_ADD, conn_sock,
+			   &ev) == -1) {
+		   perror("epoll_ctl: conn_sock");
+		   exit(EXIT_FAILURE);
+	       }
+	       printf("%d is the new socket", conn_sock);
+	       FD_SET(conn_sock, &master);
+	   } else {
+	       do_use_fd(epollfd, ev, events[n].data.fd, master, &fdmax, listener);
+	   }
+       }
+   }
+}
 
 int main() {
     int sockfd;
@@ -107,13 +186,13 @@ int main() {
         fprintf(stderr, "selectserver: %s\n", gai_strerror(rv));
         exit(1);
     }
-    
+
     for(p = ai; p != NULL; p = p->ai_next) {
         listener = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
-        if (listener < 0) { 
+        if (listener < 0) {
             continue;
         }
-        
+
         // lose the pesky "address already in use" error message
         setsockopt(listener, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
 
@@ -139,7 +218,7 @@ int main() {
     fdmax = listener;
 
     /*---Wait for socket connect to complete---*/
-	
+
      epollfd = epoll_create1(0);
    if (epollfd == -1) {
        perror("epoll_create1");
@@ -153,41 +232,15 @@ int main() {
        exit(EXIT_FAILURE);
    }
 
-   for (;;) {
-       nfds = epoll_wait(epollfd, events, MAX_EVENTS, -1);
-       if (nfds == -1) {
-	   perror("epoll_wait");
-	   exit(EXIT_FAILURE);
-       }
+   pthread_t worker;
+   worker_t *args = malloc(sizeof args);
+   args->epollfd = epollfd;
+   args->remoteaddr = remoteaddr;
+   args->fdmax = fdmax;
+   args->listener = listener;
+   args->ev = ev;
+   args->master = master;
+   pthread_create(&worker, NULL, run_worker, args);
+   pthread_join(worker, NULL);
 
-       for (n = 0; n < nfds; ++n) {
-	   if (events[n].data.fd == listener) {
-	       printf("Socket ready\n");
-	       addrlen = sizeof remoteaddr;
-	       conn_sock = accept(listener,
-		(struct sockaddr *)&remoteaddr,
-		&addrlen);
-	       if (conn_sock == -1) {
-		   perror("accept");
-		   exit(EXIT_FAILURE);
-	       }
-	       ev.events = EPOLLIN | EPOLLET;
-	       ev.data.fd = conn_sock;
-	       setunblocking(conn_sock);
-	       if (conn_sock > fdmax) {
-			fdmax = conn_sock;
-	       }
-	       if (epoll_ctl(epollfd, EPOLL_CTL_ADD, conn_sock,
-			   &ev) == -1) {
-		   perror("epoll_ctl: conn_sock");
-		   exit(EXIT_FAILURE);
-	       }
-	       printf("%d is the new socket", conn_sock);
-	       FD_SET(conn_sock, &master);
-	   } else {
-	       do_use_fd(events[n].data.fd, master, &fdmax, listener);
-	   }
-       }
-   }
 }
-
